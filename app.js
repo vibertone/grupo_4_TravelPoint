b598405f7c4153e5bd8665181dac9a2d3901929b
felipezakin <felipezakin@gmail.com>
2021-10-21 13:55:48 -0300

    modifique app.js para que corra tmbn en Mac

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const express = require("express");
-const path = require ("path");
 const app = express();
+const path = require("path");
 
 const publicPath = path.join(__dirname,'./public');
 app.use(express.static(publicPath));
 
-app.get("/index", (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/index.html'))
+
+app.listen(5000, ()=> {console.log("Levantando un servidor con Express")});
+
+app.get("/", (req, res) => {
+    let index = path.resolve(__dirname, "views/index.html");
+    res.sendFile(index)
 });
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/index.html'))
-});
+app.get("/register", (req, res) => {
+    let register = path.resolve(__dirname, "views/register.html");
+    res.sendFile(register)
+})
 
-app.get("/login", (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/login.html'))
-});
+app.get("/login", (req, res) => {
+    let login = path.resolve(__dirname, "views/login.html");
+    res.sendFile(login)
+})
 
-app.get("/productCart", (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/productCart.html'))
-});
+app.get("/productCart", (req, res) => {
+    let productCart = path.resolve(__dirname, "views/productCart.html");
+    res.sendFile(productCart)
+})
 
-app.get("/productDetail", (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/productDetail.html'))
-});
-
-app.get("/register", (req,res)=>{
-    res.sendFile(path.join(__dirname, './views/register.html'))
-});
-app.listen(5000, ()=>{
-    console.log ("Levantando un servidor con Express");
-
-});
+app.get("/productDetail", (req, res) => {
+    let productDetail = path.resolve(__dirname, "views/productDetail.html");
+    res.sendFile(productDetail)
+})
