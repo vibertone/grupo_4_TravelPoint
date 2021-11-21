eb4b849f8f6f072b40358e2c0fb696299de07720
fedebarrionuevo <fede.barrionuevo@hotmail.com>
2021-11-21 03:43:23 -0300

    agregue flights.json

--- controllers/index_ctrl.js
+++ controllers/index_ctrl.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const flightsFilePath = path.join(__dirname, '../data/flights.json');
+const flights = JSON.parse(fs.readFileSync(flightsFilePath, 'utf-8'));
+
 const controllers = {
     index: (req, res) => {
-        res.render('index')
+        res.render('index', {data: flights})
     }
 };
 module.exports = controllers;
