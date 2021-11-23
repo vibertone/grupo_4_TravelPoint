5ff090f11851cf73d4be523e84543b3c733bbf88
Victoria Bertone <Vbertone@fxinformatica.com>
2021-11-23 20:14:16 -0300

    Crud

--- controllers/productList_ctrl.js
+++ controllers/productList_ctrl.js
@@ -1,7 +1,47 @@
+const fs = require('fs');
+const path=require('path');
+const productsFilePath = path.join(__dirname, '../data/flights.json');
+
 const controllers = {
+    // Root - Show all products
     productList: (req, res) => {
         res.render('productList');
     },
+    // Detail - Detail from one product
+    detail: (req, res) => {
+		idProd=req.params.id;
+		producto = products.find(function(producto){
+			return producto.id==idProd;
+		})
+		res.render('detail',{producto})
+	},
+    // Create - Form to create
+create: (req, res) => {
+    res.render('product-create-form')
+},
+
+    // Create -  Method to store
+store: (req, res) => {
+    // Do the magic
+},
+
+    // Update - Form to edit
+edit: (req, res) => {
+    res.render('product-edit-form')
+},
+    // Update - Method to update
+update: (req, res) => {
+    res.render('product-edit-form')
+},
+
+// Delete - Delete one product from DB
+destroy : (req, res) => {
+    res.render('product-edit-form')
+}
+
 };
+
+
+
 
 module.exports = controllers;
--- routes/productList.js
+++ routes/productList.js
@@ -5,4 +5,24 @@ const controllers = require("../controllers/productList_ctrl");
 
 router.get(ENDPOINTS.PRODUCTLIST, controllers.productList);
 
+/*** LISTADO DE PRODUCTOS ***/ 
+router.get('/products', controllers.products); 
+
+/*** Formulario de creación de productos + Acción de creación***/ 
+router.get('/products/create', controllers.create); 
+router.post('/products', controllers.store); 
+
+
+/*** Detalle de un producto particular ***/ 
+router.get('/products/:id/', controllers.detail); 
+
+/*** EDITAR UN PRODUCTO ***/ 
+router.get('/products/:id/edit', controllers.edit); 
+router.put('/products/:id', controllers.update); 
+
+
+/*** DELETE UN PRODUCTO***/ 
+router.delete('/products/:id', controllers.destroy); 
+
+
 module.exports = router;
