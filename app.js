f1da3e922dc2afabfc5552fd311af0dca282bffe
Victoria Bertone <Vbertone@fxinformatica.com>
2021-11-23 23:40:31 -0300

    Draft cambios unificación de controladores

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors")
-const controllers = require("./controllers/index_ctrl");
 const methodOverride = require("method-override");
 
 app.use(methodOverride('_method'));
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const indexRoutes = require("./routes/index");
 const loginRoutes = require("./routes/login");
 const registerRoutes = require("./routes/register")
-const productDetailRoutes = require("./routes/productDetail");
-const productCartRoutes = require("./routes/productCart");
+//const productDetailRoutes = require("./routes/productDetail");
+//const productCartRoutes = require("./routes/productCart");
 const flightDetail = require("./routes/flightDetail");
-const productListRoutes = require("./routes/productList");
-const usersListRoutes = require("./routes/usersList");
+//const productListRoutes = require("./routes/productList");
+//const usersListRoutes = require("./routes/usersList");
 const myAccountRoutes = require("./routes/myAccount")
+const productRoutes = require("./routes/products")
 
 app.use(cors());
 app.use(express.static('./public'));
@@ -29,15 +28,18 @@ app.listen(5000, () => {
 app.set("view engine", "ejs");
 app.set('views', path.resolve(__dirname, "views"));
 
-app.use(('/', indexRoutes));
-app.use(('/login'), loginRoutes);
-app.use(('/register'), registerRoutes);
-app.use(('/productDetail'), productDetailRoutes);
-app.use(('/productCart'), productCartRoutes);
-app.use(('/flightDetail'), flightDetail);
-app.use(('/productList'), productListRoutes);
-app.use(('/usersList'), usersListRoutes);
-app.use(('/myaccount'), myAccountRoutes);
+app.use('/', indexRoutes);
+app.use('/login', loginRoutes);
+app.use('/register', registerRoutes);
+app.use('/productDetail', productDetailRoutes);
+//app.use(('/productCart'), productCartRoutes);
+app.use('/flightDetail', flightDetail);
+//app.use(('/productList'), productListRoutes);
+//app.use(('/usersList'), usersListRoutes);
+app.use('/myaccount', myAccountRoutes);
+
+app.use('/products', productRoutes);   
+
 
 app.use((req, res, next) => {
     res.status(404).render('not-found')
--- routes/myAccount.js
+++ routes/myAccount.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const ENDPOINTS = require("../endpoints");
 const controllers = require("../controllers/users_ctrl");
 
-router.get(ENDPOINTS.MYACOOUNT, controllers.myAccount);
+router.get(ENDPOINTS.MYACCOUNT, controllers.myAccount);
 
 module.exports = router;
