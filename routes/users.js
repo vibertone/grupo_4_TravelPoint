cd9d13d7bb0989f2e8ed5a3538e2ab5472f3504d
fedebarrionuevo <fede.barrionuevo@hotmail.com>
2021-12-03 18:14:38 -0300

    sigo con express-validator

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const {body} = require('express-validator');
 const validations = [
     body('email').notEmpty().withMessage('Tienes que escribir un mail'),
     body('password').notEmpty().withMessage('Tienes que crear una contraseña'),
+    body('repeatPassword').notEmpty().withMessage('Tienes que repetir la contraseña creada'),
     body('name').notEmpty().withMessage('Tienes que escribir tu nombre'),
     body('lastName').notEmpty().withMessage('Tienes que escribir tu apellido')
 ]
