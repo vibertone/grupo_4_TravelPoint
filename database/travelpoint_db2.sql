a253a5b45ee204bfc5cd1f6077267481be1b54b4
fedebarrionuevo <fede.barrionuevo@hotmail.com>
2022-02-17 22:25:47 -0300

    sql

--- database/travelpoint_db2.sql
+++ database/travelpoint_db2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-02-2022 a las 04:48:43
+-- Tiempo de generación: 18-02-2022 a las 02:23:52
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `travelpoint_db2`
 --
-CREATE DATABASE IF NOT EXISTS `travelpoint_db2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `travelpoint_db2`;
 
 -- --------------------------------------------------------
 
@@ -29,47 +27,54 @@ USE `travelpoint_db2`;
 -- Estructura de tabla para la tabla `airlines`
 --
 
-DROP TABLE IF EXISTS `airlines`;
 CREATE TABLE `airlines` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
+  `airline` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELACIONES PARA LA TABLA `airlines`:
---
 
 --
 -- Volcado de datos para la tabla `airlines`
 --
 
-INSERT INTO `airlines` (`id`, `name`) VALUES
+INSERT INTO `airlines` (`id`, `airline`) VALUES
 (1, 'LATAM'),
 (2, 'American Airlines'),
-(3, 'KLM');
+(3, 'KLM'),
+(4, 'Qatar Airways'),
+(5, 'Emirates'),
+(6, 'Japan Airlines'),
+(7, 'Air France'),
+(8, 'Avianca'),
+(9, 'United Airlines'),
+(10, 'Aerolíneas Argentinas'),
+(11, 'Delta Air Lines'),
+(12, 'Lufthansa'),
+(13, 'GOL'),
+(14, 'British Airways'),
+(15, 'JetSmart'),
+(16, 'Iberia'),
+(17, 'ANDES'),
+(18, 'Turkish Airlines'),
+(19, 'Austrian Airlines'),
+(20, 'Air New Zealand');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `airport`
+-- Estructura de tabla para la tabla `airports`
 --
 
-DROP TABLE IF EXISTS `airport`;
-CREATE TABLE `airport` (
+CREATE TABLE `airports` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `code` varchar(3) NOT NULL
+  `airport` varchar(200) NOT NULL,
+  `code` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELACIONES PARA LA TABLA `airport`:
+-- Volcado de datos para la tabla `airports`
 --
 
---
--- Volcado de datos para la tabla `airport`
---
-
-INSERT INTO `airport` (`id`, `name`, `code`) VALUES
+INSERT INTO `airports` (`id`, `airport`, `code`) VALUES
 (1, 'Ezeiza', 'EZE');
 
 -- --------------------------------------------------------
@@ -78,15 +83,10 @@ INSERT INTO `airport` (`id`, `name`, `code`) VALUES
 -- Estructura de tabla para la tabla `cities`
 --
 
-DROP TABLE IF EXISTS `cities`;
 CREATE TABLE `cities` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
+  `city` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELACIONES PARA LA TABLA `cities`:
---
 
 -- --------------------------------------------------------
 
@@ -94,15 +94,18 @@ CREATE TABLE `cities` (
 -- Estructura de tabla para la tabla `countries`
 --
 
-DROP TABLE IF EXISTS `countries`;
 CREATE TABLE `countries` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
+  `country` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELACIONES PARA LA TABLA `countries`:
+-- Volcado de datos para la tabla `countries`
 --
+
+INSERT INTO `countries` (`id`, `country`) VALUES
+(4, 'Argentina'),
+(91, 'Bolivia');
 
 -- --------------------------------------------------------
 
@@ -110,24 +113,13 @@ CREATE TABLE `countries` (
 -- Estructura de tabla para la tabla `destinations`
 --
 
-DROP TABLE IF EXISTS `destinations`;
 CREATE TABLE `destinations` (
   `id` int(11) NOT NULL,
-  `city_code` varchar(20) NOT NULL,
+  `code_id` int(11) NOT NULL,
   `airport_id` int(11) NOT NULL,
   `city_id` int(11) NOT NULL,
   `country_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELACIONES PARA LA TABLA `destinations`:
---   `airport_id`
---       `airport` -> `id`
---   `city_id`
---       `cities` -> `id`
---   `country_id`
---       `countries` -> `id`
---
 
 -- --------------------------------------------------------
 
@@ -135,7 +127,6 @@ CREATE TABLE `destinations` (
 -- Estructura de tabla para la tabla `flights`
 --
 
-DROP TABLE IF EXISTS `flights`;
 CREATE TABLE `flights` (
   `id` int(11) NOT NULL,
   `flight_number` varchar(50) NOT NULL,
@@ -144,21 +135,12 @@ CREATE TABLE `flights` (
   `itinerary_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELACIONES PARA LA TABLA `flights`:
---   `airline_id`
---       `airlines` -> `id`
---   `itinerary_id`
---       `itineraries` -> `id`
---
-
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `itineraries`
 --
 
-DROP TABLE IF EXISTS `itineraries`;
 CREATE TABLE `itineraries` (
   `id` int(11) NOT NULL,
   `date` date NOT NULL,
@@ -167,38 +149,19 @@ CREATE TABLE `itineraries` (
   `destiny_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELACIONES PARA LA TABLA `itineraries`:
---   `destiny_id`
---       `destinations` -> `id`
---   `origin_id`
---       `origins` -> `id`
---
-
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `origins`
 --
 
-DROP TABLE IF EXISTS `origins`;
 CREATE TABLE `origins` (
   `id` int(11) NOT NULL,
-  `city_code` varchar(20) NOT NULL,
+  `code_id` int(11) NOT NULL,
   `airport_id` int(11) NOT NULL,
   `city_id` int(11) NOT NULL,
   `country_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELACIONES PARA LA TABLA `origins`:
---   `airport_id`
---       `airport` -> `id`
---   `city_id`
---       `cities` -> `id`
---   `country_id`
---       `countries` -> `id`
---
 
 -- --------------------------------------------------------
 
@@ -206,11 +169,10 @@ CREATE TABLE `origins` (
 -- Estructura de tabla para la tabla `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `lastName` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
   `email` varchar(150) NOT NULL,
   `country_id` int(11) NOT NULL,
   `image` varchar(500) NOT NULL,
@@ -218,27 +180,12 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELACIONES PARA LA TABLA `users`:
---   `country_id`
---       `countries` -> `id`
---
-
---
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `country_id`, `image`, `password`) VALUES
-(12, 'Carla', 'Sanchez', 'carla@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$qD0Qw2bS9FUw4rH/ZRW3u.2LOa5Ol.YnPjhVZtvvbJCtHskMwkWaS'),
-(13, 'Federico', 'Mansilla', 'fede@hotmail.com', 0, 'bde4e168-74d7-4cbe-9bfa-6d58711c4683.valderrama.jpg', '$2a$10$1eProIidg2mV1U05bx6joOqjVaQwrZJzYftIjH2XnUr8zbTU1fu5K'),
-(14, 'Marcos', 'Ramirez', 'marcos@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$mKKvQFW0L7O3T5jADGzOSeYNZRnxpM74rUsmo1dFuHKkqidHKZhpW'),
-(15, 'Juana', 'Gomez', 'juana@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$TjgmwJ43FxMROltU2TKF3.hKcEKJV7sgypKA8Apd6lPOvYWCkAEdi'),
-(16, 'Maria', 'Martinez', 'maria@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$apmBUpeGSY3zMdNCUa6wkutXAi8Wi2R3SORuTXPU.Ta1Wz.0dNbEy'),
-(17, 'Lucas', 'Lopez', 'lucas@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$xWr/Q2ldWaw9U6i4ky6L/.wfT71TcnFOrp53nU4ZZDIVxZSixM3A.'),
-(18, 'Lucia', 'Rivas', 'lucia@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$vJdVZ5oGoVA887sMaUJF4udY9/q2KuZ/99dAWJn2l0UP6yO408yUS'),
-(39, 'Jose', 'Gomez', 'jose@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$1qaKlGclWTTEfr.JcNh6fe7SJ3jkcBwGZQbfjBIWGELr5pdH8yO2G'),
-(43, 'Marcelo', 'Fontana', 'marcelo@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$VhQh0ASF9lLNTNFYtU7XdObD6LOlx7ykmBRCxEUhiJq6Jj/GvpPmW'),
-(46, 'Julia', 'Sanchez', 'julia@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$cAwrQxcnPYj30EcAecKeheVOJ7J5i2ro5gCYBTqUhHhVnnuIAW17.'),
-(47, 'Carlos', 'Gutierrez', 'carlos@hotmail.com', 0, '/perfil-sin-foto.jpg', '$2a$10$KV8zmrPBMLmS8d6OA64blOJyf/fQEyuYfUqFjVuYc0y5ZKl4xuldW');
+INSERT INTO `users` (`id`, `name`, `last_name`, `email`, `country_id`, `image`, `password`) VALUES
+(48, 'Federico', 'Garcia', 'fede@hotmail.com', 4, 'b8246934-49b1-4c2a-a344-238d63f21e65.valderrama.jpg', '$2a$10$5.xVzH8zpHqHJj.5uwa8L.JcXMHEkv8B3whpkF8BJO5VlBlpdI4tS'),
+(49, 'Carlos', 'Ramirez', 'carlos@hotmail.com', 91, '/perfil-sin-foto.jpg', '$2a$10$MCsbFYtdxvU/GLjsTEQeUeqwxjE81zexjFB5BXiMTRu459msAlNb6');
 
 -- --------------------------------------------------------
 
@@ -246,20 +193,10 @@ INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `country_id`, `image`, `
 -- Estructura de tabla para la tabla `user_itinerary`
 --
 
-DROP TABLE IF EXISTS `user_itinerary`;
 CREATE TABLE `user_itinerary` (
-  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `itinerary_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELACIONES PARA LA TABLA `user_itinerary`:
---   `itinerary_id`
---       `itineraries` -> `id`
---   `user_id`
---       `users` -> `id`
---
 
 --
 -- Índices para tablas volcadas
@@ -272,9 +209,9 @@ ALTER TABLE `airlines`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `airport`
+-- Indices de la tabla `airports`
 --
-ALTER TABLE `airport`
+ALTER TABLE `airports`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -293,47 +230,117 @@ ALTER TABLE `countries`
 -- Indices de la tabla `destinations`
 --
 ALTER TABLE `destinations`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `airport_code_id` (`code_id`,`airport_id`,`city_id`,`country_id`),
+  ADD KEY `code_id` (`code_id`),
+  ADD KEY `city_id` (`city_id`),
+  ADD KEY `country_id` (`country_id`),
+  ADD KEY `airport_id` (`airport_id`);
 
 --
 -- Indices de la tabla `flights`
 --
 ALTER TABLE `flights`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `airline_id` (`airline_id`,`itinerary_id`),
+  ADD KEY `itinerary_id` (`itinerary_id`);
 
 --
 -- Indices de la tabla `itineraries`
 --
 ALTER TABLE `itineraries`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `origin_id` (`origin_id`,`destiny_id`),
+  ADD KEY `destiny_id` (`destiny_id`);
 
 --
 -- Indices de la tabla `origins`
 --
 ALTER TABLE `origins`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `code_id` (`code_id`,`airport_id`,`city_id`,`country_id`),
+  ADD KEY `city_id` (`city_id`),
+  ADD KEY `airport_id` (`airport_id`),
+  ADD KEY `country_id` (`country_id`);
 
 --
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `country_id` (`country_id`);
 
 --
 -- Indices de la tabla `user_itinerary`
 --
 ALTER TABLE `user_itinerary`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `user_id` (`user_id`,`itinerary_id`),
+  ADD KEY `itinerary_id` (`itinerary_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `airlines`
+--
+ALTER TABLE `airlines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `destinations`
+--
+ALTER TABLE `destinations`
+  ADD CONSTRAINT `destinations_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `destinations_ibfk_2` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `destinations_ibfk_3` FOREIGN KEY (`airport_id`) REFERENCES `airports` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `destinations_ibfk_4` FOREIGN KEY (`code_id`) REFERENCES `airports` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `flights`
+--
+ALTER TABLE `flights`
+  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`airline_id`) REFERENCES `airlines` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`itinerary_id`) REFERENCES `itineraries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `itineraries`
+--
+ALTER TABLE `itineraries`
+  ADD CONSTRAINT `itineraries_ibfk_1` FOREIGN KEY (`origin_id`) REFERENCES `origins` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `itineraries_ibfk_2` FOREIGN KEY (`destiny_id`) REFERENCES `destinations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `origins`
+--
+ALTER TABLE `origins`
+  ADD CONSTRAINT `origins_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `origins_ibfk_2` FOREIGN KEY (`airport_id`) REFERENCES `airports` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `origins_ibfk_3` FOREIGN KEY (`code_id`) REFERENCES `airports` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `origins_ibfk_4` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `user_itinerary`
+--
+ALTER TABLE `user_itinerary`
+  ADD CONSTRAINT `user_itinerary_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_itinerary_ibfk_2` FOREIGN KEY (`itinerary_id`) REFERENCES `itineraries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
